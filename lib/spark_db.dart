4e64ad3884332b82c472d7154e9d699638919b80
Alexander Adeniyin <trinoxf@gmail.com>
2014-08-07 16:29:33 +0100

    first commit

---
+++ lib/spark_db.dart
@@ -0,0 +1,17 @@
+library spark_db;
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:couchclient/couchlinet.dart';
+import 'package:path/path.dart';
+import 'package:dart_redis/dart_redis.dart';
+
+
+class SparkDB{
+
+  void register(){
+
+  }
+}
